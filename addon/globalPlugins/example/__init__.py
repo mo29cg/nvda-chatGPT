52b526ef5763fc1f65334f8b6cbfa1017ca2d018
Suzuki <milmil_13@yahoo.co.jp>
2023-03-05 18:50:55 +0900

    init

---
+++ addon/globalPlugins/example/__init__.py
@@ -0,0 +1,139 @@
+import pprint
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'site-packages'))
+
+f = open("C:\\Users\\suzuki\\1.json", "w")
+print(pprint.pformat(sys.path), file=f)
+f.close()
+import gui
+import wx
+import config
+from scriptHandler import script
+import inputCore
+import globalPluginHandler
+from revChatGPT.Official import Chatbot
+import treeInterceptorHandler
+import ui
+import textInfos
+import api
+import threading
+from openai.error import RateLimitError
+from openai.error import ServiceUnavailableError
+
+module = "workingChatGPT"
+
+
+def initConfiguration():
+    confspec = {
+        "apiKey": "string( default='')",
+        "askWordBinding": "string( default='NVDA+a')",
+        "askSentence": "string( default='NVDA+l')",
+    }
+    config.conf.spec[module] = confspec
+
+
+def getConfig(key):
+    value = config.conf[module][key]
+    return value
+
+
+def setConfig(key, value):
+    config.conf[module][key] = value
+
+
+initConfiguration()
+
+
+class OptionsPanel(gui.SettingsPanel):
+    title = _("workingChatGPT")
+
+    def makeSettings(self, settingsSizer):
+        sHelper = gui.guiHelper.BoxSizerHelper(self, sizer=settingsSizer)
+
+        self.apiKey = sHelper.addLabeledControl(
+            _("chatGPT api key:"), wx.TextCtrl)
+        self.apiKey.Value = getConfig("apiKey")
+
+        self.askWordBinding = sHelper.addLabeledControl(
+            _("key binding for ask wword: "), wx.TextCtrl)
+
+        self.askWordBinding.Value = getConfig("askWordBinding")
+
+        self.askSentence = sHelper.addLabeledControl(
+            _("key binding for ask sentence:"), wx.TextCtrl)
+        self.askSentence.Value = getConfig("askSentence")
+
+    def onSave(self):
+        setConfig("apiKey", self.apiKey.Value)
+        setConfig("askSentence", self.askSentence.Value)
+        ui.message("You need to restart nvda to take effect")
+
+# this way, it can get selected text from anywhere
+
+
+def get_selected_text():
+    obj = api.getFocusObject()
+    treeInterceptor = obj.treeInterceptor
+    if isinstance(treeInterceptor, treeInterceptorHandler.DocumentTreeInterceptor):
+        obj = treeInterceptor
+
+    try:
+        info = obj.makeTextInfo(textInfos.POSITION_SELECTION)
+    except (RuntimeError, NotImplementedError):
+        return ""
+    return info.text.strip()
+
+
+def askChatGPT(text):
+    ui.message("asking to chatGPT")
+
+    chatbot = Chatbot(
+        api_key=getConfig("apiKey"))
+    try:
+        response = chatbot.ask(text)
+        ui.browseableMessage(response["choices"][0]["text"])
+    except (RateLimitError, ServiceUnavailableError):
+        ui.message("the server is busy, try again later")
+
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+    def __init__(self):
+        super(GlobalPlugin, self).__init__()
+        gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(
+            OptionsPanel)
+
+    def terminate(self):
+        super(GlobalPlugin, self).terminate()
+        gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove(
+            OptionsPanel)
+
+    @script(
+        description=_("ask the meaning of a word to chatGPT"),
+        gestures=["kb:" + getConfig("askWordBinding")]
+    )
+    def script_askMeaningOfWord(self, gesture):
+        selectedText = get_selected_text()
+        if len(selectedText) == 0:
+            ui.message("no selection")
+            return
+
+        threading1 = threading.Thread(
+            target=askChatGPT, args=(selectedText + "とはどういう意味ですか、返答は日本語でお願いします",))
+        threading1.start()
+
+    @script(
+        description=_("ask the meaning of a word to chatGPT"),
+        gestures=["kb:" + getConfig("askSentence")]
+        # gestures=["kb:NVDA+l"]
+    )
+    def script_askSentence(self, gesture):
+        selectedText = get_selected_text()
+        if len(selectedText) == 0:
+            ui.message("no selection")
+            return
+
+        threading1 = threading.Thread(
+            target=askChatGPT, args=(selectedText,))
+        threading1.start()
---
+++ addon/globalPlugins/example/site-packages/revChatGPT/GPTserver.py
@@ -0,0 +1,84 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from revChatGPT.ChatGPT import Chatbot
+
+app = Flask(__name__)
+
+# Session token based rate limiting
+token_available: dict = {}
+
+chatbot = Chatbot(config={}, conversation_id=None, parent_id=None, no_refresh=True)
+
+
+def verify_data(data: dict) -> bool:
+    """
+    Verifies that the required fields are present in the data.
+    """
+    # Required fields: "prompt", "session_token"
+    if "prompt" not in data or "session_token" not in data:
+        return False
+    return True
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    """
+    The main chat endpoint.
+    """
+    data: dict = request.get_json()
+    if not verify_data(data=data):
+        return jsonify({"error": "Invalid data."}), 400
+
+    chatbot.session_token: str = data["session_token"]
+
+    conversation_id = data.get("conversation_id", None)
+    parent_id = data.get("parent_id", None)
+
+    # Return rate limit if token_available is false
+    if token_available.get(
+        data.get("session_token"),
+    ) != None and not token_available.get(data.get("session_token")):
+        return jsonify({"error": "Rate limited"}), 429
+
+    token_available[data.get("session_token")] = False
+
+    try:
+        response = chatbot.ask(
+            prompt=data["prompt"],
+            session_token=data["session_token"],
+            parent_id=parent_id,
+            conversation_id=conversation_id,
+        )
+    except Exception as exc:
+        token_available[data.get("session_token")] = True
+        return jsonify({"error": str(exc)}), 500
+
+    response["session_token"] = chatbot.session_token
+
+    token_available[data.get("session_token")] = True
+
+    return jsonify(response), 200
+
+
+@app.route("/refresh", methods=["POST"])
+def refresh():
+    """
+    The refresh endpoint.
+    """
+    data = request.get_json()
+    if "session_token" not in data:
+        return jsonify({"error": "Invalid data."}), 400
+    if not token_available.get(data.get("session_token")):
+        return jsonify({"error": "Invalid token."}), 400
+    chatbot.session_token = data["session_token"]
+    try:
+        chatbot.refresh_session()
+    except Exception as exc:
+        return jsonify({"error": str(exc)}), 400
+
+    return jsonify({"session_token": chatbot.session_token}), 200
+
+
+def main():
+    app.run(host="127.0.0.1", port=8080)
