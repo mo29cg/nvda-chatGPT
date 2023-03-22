0ffc25d5a0a856d997b1c76b406763862286e755
Suzuki <milmil_13@yahoo.co.jp>
2023-03-22 17:25:38 +0900

    Update NVDA version in buildVars.py

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
     # Translators: Long description to be shown for this add-on on add-on information from add-ons manager
     "addon_description": _("""provider an access to chatGPT"""),
     # version
-    "addon_version": "0.3",
+    "addon_version": "0.4",
     # Author(s)
     "addon_author": "suzukix<lcong5946@gmail.com>",
     # URL for the add-on documentation support
@@ -35,9 +35,9 @@ addon_info = {
     # Documentation file name
     "addon_docFileName": "readme.html",
     # Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-    "addon_minimumNVDAVersion": 2022.1,
+    "addon_minimumNVDAVersion": 2023.1,
     # Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-    "addon_lastTestedNVDAVersion": 2022.1,
+    "addon_lastTestedNVDAVersion": 2023.1,
     # Add-on update channel (default is None, denoting stable releases,
     # and for development releases, use "dev".)
     # Do not change unless you know what you are doing!
