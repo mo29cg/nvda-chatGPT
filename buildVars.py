5445dabdb7afec40eec6fd35ef2e9279d004f4bd
Suzuki <milmil_13@yahoo.co.jp>
2024-07-05 17:16:24 +0900

    bump version

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""provide an access to chatGPT"""),
 	# version
-	"addon_version": "0.22",
+	"addon_version": "0.25",
 	# Author(s)
 	"addon_author": "suzukix<lcong5946@gmail.com>",
 	# URL for the add-on documentation support
